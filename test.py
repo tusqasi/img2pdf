cff55a33789df6ebc29c38ef48f15ee4a208b5ae
tusqasi <you@example.com>
2021-05-18 00:31:00 +0530

    path correction

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ def main():
     im_list = [Image.open(f"./{x}").convert("RGB") for x in imgs[1:]]
     im1 = Image.open(imgs[0]).convert("RGB")
     pdf1_filename = f"{sys.argv[0]}.pdf"
-    print(sys.argv[0])
+    print(sys.argv)
 
     im1.save(
         pdf1_filename, "PDF", resolution=100.0, save_all=True, append_images=im_list
