4029854923f0bc1246062abbe7f43ad51614b5d5
tusqasi <you@example.com>
2021-05-18 00:36:02 +0530

    path correction

--- test.py
+++ test.py
@@ -4,17 +4,17 @@ from PIL import Image
 
 
 def main():
-    all_files = os.listdir("./hw")
+    folder = sys.argv[1]
+    all_files = os.listdir(f"./{folder}")
     extensions = [
         "jpg",
         "jpeg",
         "png",
     ]
-    imgs = [f"hw/{x}" for x in all_files if x[-3:].lower() in extensions]
+    imgs = [f"{folder}/{x}" for x in all_files if x[-3:].lower() in extensions]
     im_list = [Image.open(f"./{x}").convert("RGB") for x in imgs[1:]]
     im1 = Image.open(imgs[0]).convert("RGB")
     pdf1_filename = f"{sys.argv[0]}.pdf"
-    print(sys.argv)
 
     im1.save(
         pdf1_filename, "PDF", resolution=100.0, save_all=True, append_images=im_list
